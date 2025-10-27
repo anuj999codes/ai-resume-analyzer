a05a580b206ed3b2be8b88be50c59fb4946a08e9
Anuj Tailor <anujtailor2806@gmail.com>
2025-10-27 19:11:24 +0530

    resume feedback components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,6 @@
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
 export function formatSize(bytes: number): string {
   if (bytes === 0) return "0 Bytes";
   const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
@@ -10,4 +13,8 @@ export function formatSize(bytes: number): string {
 
 export const generateUUID = () => {
     return crypto.randomUUID();
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
